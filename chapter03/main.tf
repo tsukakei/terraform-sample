72c35f940b6dcb07efba797f44c64dd1a481ae25
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-10-21 18:23:01 +0900

    use dynamodb for state locking

--- chapter03/main.tf
+++ chapter03/main.tf
@@ -5,6 +5,7 @@ terraform {
     region = "us-east-1"
     key = "terraform.tfstate"
     encrypt = true
+    dynamodb_table = "terraform_state_lock"
   }
 }
 
@@ -16,5 +17,17 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "tsukakei-example-terraform-state"
   versioning {
     enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name = "terraform_state_lock"
+  read_capacity = 1
+  write_capacity = 1
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
