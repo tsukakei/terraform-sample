7d07d3686f86f0bf5562802405c70356fcd8b002
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-10-21 14:33:50 +0900

    add load balancers

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_availability_zones" "all" {}
+
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
@@ -51,6 +53,10 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -59,5 +65,44 @@ resource "aws_autoscaling_group" "example" {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
