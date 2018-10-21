d331e954f5fda04476b0044e926b1f92437643ec
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-10-21 13:55:57 +0900

    use var and output

--- main.tf
+++ main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #! /bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
 
   tags {
     Name = "terrafrorm-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
